11bbb7caf1f079c7507fbdb2452a57ea9efd9e1f
Gyohdon <loco_crazy_guy@hotmail.com>
2016-08-09 21:21:12 +0200

    no message

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var Botkit = require('botkit')
+var Botkit = require('botkit');
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
+var slackToken = process.env.SLACK_TOKEN;
 if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
+  console.error('SLACK_TOKEN is required!');
+  process.exit(1);
 }
 
-var controller = Botkit.slackbot()
+var controller = Botkit.slackbot();
 var bot = controller.spawn({
-  token: slackToken
+  token: slackToken;
 })
 
 bot.startRTM(function (err, bot, payload) {
   if (err) {
-    throw new Error('Could not connect to Slack')
+    throw new Error('Could not connect to Slack');
   }
-})
+});
 
 controller.hears('.*', ['mention', 'direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'You are being hysterical.')
-})
+  bot.reply(message, 'You are being hysterical.');
+});
