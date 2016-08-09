d3e56362f17cb80d7b1faa25b2a448b07f7b0091
Gyohdon <loco_crazy_guy@hotmail.com>
2016-08-09 21:16:38 +0200

    little bot test

---
+++ index.js
@@ -0,0 +1,23 @@
+var Botkit = require('botkit')
+
+// Expect a SLACK_TOKEN environment variable
+var slackToken = process.env.SLACK_TOKEN
+if (!slackToken) {
+  console.error('SLACK_TOKEN is required!')
+  process.exit(1)
+}
+
+var controller = Botkit.slackbot()
+var bot = controller.spawn({
+  token: slackToken
+})
+
+bot.startRTM(function (err, bot, payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+})
+
+controller.hears('.*', ['mention', 'direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, 'You are being hysterical.')
+})
